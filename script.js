606693bdf5c7f899f33b95ff629ca24c042de6da
Federico Barberon <66987826+FedericoBarberon@users.noreply.github.com>
2022-04-16 15:29:29 -0300

    upload

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
-const visor = document.getElementById("visor");
+const visorActual = document.getElementById("actual");
+const visorPrevious = document.getElementById("previous");
+const visorOperation = document.getElementById("operation");
 const numbers = document.getElementById("numbers");
 const operators = document.getElementById("operators");
-let operation;
+let operator;
+
+const operations = {
+    clear: () => {
+        visorActual.textContent = '';
+        visorPrevious.textContent = '';
+        operator = null;
+    },
+    
+    clearNumber: () => {
+        visorActual.textContent = visorActual.textContent.slice(0,-1);
+    },
+
+    addition: (a=0,b=0) => a+b,
+    subtraction: (a=0,b=0) => a-b,
+    multiplication: (a=1,b=1) => a*b,
+    division: (a=1,b=1) => a/b,
+
+    changeSign: () => {
+        if(visorActual.textContent){
+            visorActual.textContent = (parseFloat(visorActual.textContent)* - 1).toString();
+        } else {
+            visorActual.textContent += '-';
+        }
+    } 
+}
 
 numbers.addEventListener("click", ev => {
     if(ev.target.classList.contains("number")){
-        visor.textContent += ev.target.textContent;
+        if(operator === 'equal') {
+            visorPrevious.textContent = '';
+            operator = null;
+        }
+        const number = ev.target.textContent;
+        visorActual.textContent += number;
     }
 });
 
 operators.addEventListener("click", ev => {
-    let button = ev.target.textContent;
     if(ev.target.classList.contains("operator")){
-        if(button === "="){
-            operation = visor.textContent;
 
-            operate(operation);
-        } else if(button === "C") {
-            visor.textContent = visor.textContent.slice(0, visor.textContent.length - 1);
+        if(ev.target.dataset.op === 'changeSign' || ev.target.dataset.op === 'clearNumber'){
+            operations[ev.target.dataset.op]();
         }
-        else if(button === "CE") {
-            visor.textContent = "";
-        }
-        else {
-            visor.textContent += button;
+        else if(ev.target.dataset.op === 'clear'){
+            operations[ev.target.dataset.op]();
+            visorOperation.textContent = '';
+        } else {
+            if(visorPrevious.textContent) {
+                if(operator !== 'equal'){
+                    let a = parseFloat(visorPrevious.textContent);
+                    let b = parseFloat(visorActual.textContent);
+
+                    if(!isNaN(b)){
+                        const result = operations[operator](a,b).toString();
+        
+                        visorActual.textContent = '';
+                        visorPrevious.textContent = result;
+                        visorOperation.textContent = '';
+                    }
+    
+                }
+            } else {
+                visorPrevious.textContent = visorActual.textContent;
+                visorActual.textContent = '';
+            }
+            
+            operator = ev.target.dataset.op;
+            if(operator !== 'equal') visorOperation.textContent = ev.target.textContent;
         }
     }
 })
-
-function operate(op) {
-    visor.textContent = "";
-}
